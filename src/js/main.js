030dd651999290e911b81b78715300a0ea949bfa
dpw1 <diego.boarutto.fortes@gmail.com>
2019-01-09 23:29:49 +0000

    [ADD] user can download images now

--- src/js/main.js
+++ src/js/main.js
@@ -3,6 +3,7 @@
 		$(function() {
 			const $button = document.querySelector('#sendImages');
 			const $fileInput = document.querySelector('#imageInput');
+			const $watermarkInput = document.querySelector('#watermarkInput');
 			const $watermark = document.querySelector('#watermark');
 			const $carousel = document.querySelector('.images-list');
 			const $watermarkBox = document.querySelector('#wm-box')
@@ -11,23 +12,27 @@
 			/* TODO: make preview for thumbnail */
 			var imagesPreview = function(input, whereToInsertImage, htmlItem, _callback) {
 				whereToInsertImage.innerHTML = '';
+				var newHtml = '';
 				if (input.files) {
 					var filesAmount = input.files.length;
-
+					
+					console.log(filesAmount)
 					for (var i = 0; i < filesAmount; i++) {
 						var reader = new FileReader();
-
+						
 						reader.onload = function(event) {
-							htmlItem = htmlItem.replace('variable', event.target.result)
-							whereToInsertImage.insertAdjacentHTML('afterbegin', htmlItem);
+							console.log(event.target.result)
+							newHtml = htmlItem.replace('variable', event.target.result);
+							whereToInsertImage.insertAdjacentHTML('afterbegin', newHtml);
 						}
 
 						reader.readAsDataURL(input.files[i]);
+						
 					}
+					
 				}
 				_callback();
 			};
-
 			//jQuery
 			var showSelectedImages = function(){
 				$('#imageInput').on('change', function() {
@@ -54,13 +59,17 @@
 			var addWatermarkToImage = function() {
 				$button.addEventListener('click', function() {
 					var imgs = $fileInput.files;
-					var wm = $watermark.files[0];
+					var wm = $watermarkInput.files[0];
+					var wmTransparency = document.querySelector('#watermarkTransparency').value / 100;
+					var $whereToInsertImage = document.querySelector('#finalResult');
+					$whereToInsertImage.innerHTML='';
 					for (var each of imgs) {
 						var img = each;
 						watermark([img, wm])
-							.image(watermark.image.center(1))
+							.image(watermark.image.center(wmTransparency))
 							.then(function(img) {
-								document.getElementById('center').appendChild(img);
+								$whereToInsertImage.insertAdjacentHTML('afterbegin', `<div class="carousel-item"></div>`);
+								$whereToInsertImage.querySelector('.carousel-item:last-of-type').appendChild(img);
 							});
 					}
 				});
@@ -68,10 +77,15 @@
 			var startSideNav = function() {
 				$('.sidenav').sidenav();
 			};
+			var startToolTip = function(){
+				$('.tooltipped').tooltip();
+			}
 			var init = function() {
 				showSelectedImages();
 				showSelectedWatermark();
 				startSideNav();
+				startToolTip();
+				addWatermarkToImage();
 			};
 
 			init();
